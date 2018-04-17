1cffbddb63963d092712850cb2bd36dc003020e4
Yangchen Shen <shyangch@gmail.com>
2018-04-17 16:35:37 -0700

    Use modal to open and close store page on top of the home page

--- js/store.js
+++ js/store.js
@@ -1,1 +1,17 @@
+const modal = document.getElementById('modal');
 
+const openButton = document.getElementById("open-store");
+
+const closeButtons = document.getElementsByClassName("js-modal-close");
+
+openButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  modal.classList.add("is-open");
+});
+
+for (let i = 0; i < closeButtons.length; i++) {
+  closeButtons[i].onclick = function() {
+    event.preventDefault();
+    modal.classList.remove("is-open");
+  };
+}
